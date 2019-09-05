5ee9819b955f5719bade9e1c4a9edf9620ac4939
Krishnendu Saha <krishnendu@tiatros.com>
2019-09-05 13:41:15 +0530

    changes in registration package

--- src/test/java/com/qa/tiatros/registration/testcase/TC002.java
+++ src/test/java/com/qa/tiatros/registration/testcase/TC002.java
@@ -1,11 +1,9 @@
-//************* This Test Case page consist of many Test Case for registration process*****************//
-
 package com.qa.tiatros.registration.testcase;
 
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
 import com.qa.tiatros.base.TestBase;
 import com.qa.tiatros.pages.DashboardPage;
 import com.qa.tiatros.pages.FromWelcomeVidPage;
@@ -24,7 +22,7 @@ import com.qa.tiatros.pages.User_factsPage;
 import com.qa.tiatros.util.CustomListener;
 
 @Listeners(CustomListener.class)
-public class DashboardTest extends TestBase {
+public class TC002 extends TestBase {
 
 	// Global object declairation
 
@@ -43,12 +41,12 @@ public class DashboardTest extends TestBase {
 	User_factsPage uf;
 	ReturnUserRegistrationPage rur;
 
-	public DashboardTest() {
+	public TC002() {
 		super();
 	}
 
 	@BeforeMethod
-	public void initobjects() throws Exception { // This Before Method will run always````````` before any @Test.
+	public void initobjects() throws Exception { // This Before Method will run always before any @Test.
 		init();
 		rp = new RegistrationPage();
 		sp = new SignupPage();
@@ -64,61 +62,8 @@ public class DashboardTest extends TestBase {
 		rur = new ReturnUserRegistrationPage();
 	}
 
-	@Test(enabled = true)
-	public void dashboard_Click_All() throws Throwable { // This Test case will run End to END Registration without phn number
-
-		sp = rp.click_signupLink();
-		p = sp.signup_Details();
-		pp = p.verifyPin();
-		fwv = pp.clickNext();
-		d = fwv.inputProfileDetails();
-		d.verify_text7();
-
-		// Phq9 page call
-		p9 = d.phq9Question();
-		p9.verify_text8();
-		p9.phq9_Dynamic_Click();
-
-		// Gad7 page call
-		g7 = d.gad7Question();
-		g7.verify_text9();
-		g7.gad7_Dynamic_Click();
-
-		// Phq15 Page call
-		p15 = d.phq15Question();
-		p15.verify_text10();
-		p15.phq15_Dynamic_Click();
-
-		// Phq10 Page call
-		p10 = d.pss10Question();
-		p10.verify_text11();
-		p10.pss10_Dynamic_Click();
-
-		// User facts
-		uf = d.user_factsQuestion();
-		uf.verify_text13();
-		uf.user_facts_details();
-
-		// Stress Inventory Sections
-
-		si = d.stress_inventoryQuestion();
-		si.verify_text12();
-		si.stress_inventory_CheckBox();
-
-		// Pledge call
-		d.pledge();
-
-		d.scale_click();
-		d.course_complete();
-
-		// Logout Method
-		d.logoutM();
-
-	}
-
-	@Test(enabled = false)
-	public void dashboard_Click_All_Phone() throws Throwable { // This Test case will run End to END Registration with phn number included 
-
+	@Test(description = "This Test case will run End to END Registration with phn number included")
+	public void dashboard_Click_All_Phone() throws Throwable {
 		sp = rp.click_signupLink();
 		p = sp.signup_Details();
 		pp = p.verifyPin();
@@ -168,40 +113,10 @@ public class DashboardTest extends TestBase {
 
 	}
 
-	@Test(enabled = false)
-	public void click_Phq9Page() throws Throwable // This Test case will only run for Phq9 Page Questions for
-													// Registration process
-	{
-		sp = rp.click_signupLink();
-		p = sp.signup_Details();
-		pp = p.verifyPin();
-		fwv = pp.clickNext();
-		d = fwv.inputProfileDetails();
-		d.verify_text7();
-		p9 = d.phq9Question();
-		p9.verify_text8();
-		p9.phq9_static_CheckBox();
-
-	}
-
-	@Test(enabled = false)
-	public void click_Gad7Page() throws Throwable
-	// Registration process
-	{
-		sp = rp.click_signupLink();
-		p = sp.signup_Details();
-		pp = p.verifyPin();
-		fwv = pp.clickNext();
-		d = fwv.inputProfileDetails();
-		d.verify_text7();
-		g7 = d.gad7Question();
-		g7.verify_text9();
-		g7.gad7_Dynamic_Click();
-	}
-
 	@AfterMethod
 	public void tearDown() // This AfterMethod will run every time after any @Test
 	{
 		driver.quit();
 	}
+
 }
